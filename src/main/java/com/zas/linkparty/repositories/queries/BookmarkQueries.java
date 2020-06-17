2d7d4b6ec9cbeaaa3c20883bbfc001a2c9398474
ali <anili@my.bcit.ca>
2020-06-16 23:05:44 -0700

    implemented some controlle and repository methods for bookmark

--- src/main/java/com/zas/linkparty/repositories/queries/BookmarkQueries.java
+++ src/main/java/com/zas/linkparty/repositories/queries/BookmarkQueries.java
@@ -1,4 +1,13 @@
 package com.zas.linkparty.repositories.queries;
 
 public class BookmarkQueries {
+    public static String save = "insert into bookmarks " +
+            "(url, title, description, creator, group_id) " +
+            "values (?, ?, ?, ?, ?) returning bookmark_id, url, title, description, date_created, creator, group_id";
+
+    public static String findById = "select bookmark_id, url, title, description, date_created, user_id, group_id " +
+            "from bookmarks where bookmark_id = ?";
+
+    public static String findAll = "select bookmark_id, url, title, description, date_created, user_id, group_id " +
+            "from bookmarks";
 }

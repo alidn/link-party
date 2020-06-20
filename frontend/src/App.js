59edd96caf19128e68127cdfb7f02daafdfda3ba
ali <anili@my.bcit.ca>
2020-06-20 00:00:08 -0700

    Added notification component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,27 @@ import Spinner from "./components/Spinner.js";
 import AppBar from "./components/AppBar.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import {
+  Notifications,
+  getNotificationCreator,
+} from "./components/notification";
 
 function App() {
   let [isSidebarOpen, setSidebarOpen] = useState(true);
 
   let toggleSidebar = () => setSidebarOpen((isOpen) => !isOpen);
+  let [addNotif, setAddNotif] = useState(() => {});
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAddNotif(getNotificationCreator);
+    }, 0);
+  }, []);
 
   return (
     <Router>
+      <button onClick={() => addNotif("success", 2000)}>Notif</button>
+      <Notifications />
       <div>
         <AppBar toggleSidebar={toggleSidebar} />
         <div id="main">
---
+++ frontend/src/components/notification.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import "./styles/notification.css";
+import { motion } from "framer-motion";
+
+const types = {
+  success: "success",
+  error: "error",
+};
+
+let notificationCreator;
+
+export function getNotificationCreator() {
+  return notificationCreator;
+}
+
+const variants = {
+  hidden: { opacity: 0, y: -100 },
+  visible: { opacity: 1, y: 0 },
+};
+
+export function Notifications() {
+  let [list, setList] = useState([]);
+  let id = 0;
+
+  let showNotification = (type, timeoutMs) => {
+    id += 1;
+    setList((prevList) =>
+      prevList.concat({
+        id: id,
+        type: type,
+      })
+    );
+
+    // TODO: all notifications disappear at the same time.
+    setTimeout(() => {
+      let filteredList = list.filter((v) => v.id === id);
+      setList(filteredList);
+    }, timeoutMs);
+  };
+
+  useEffect(() => {
+    notificationCreator = showNotification;
+  }, []);
+
+  return (
+    <div id="notification-container">
+      {list.map((notif) => (
+        <Notification key={notif.id} type={notif.type} />
+      ))}
+    </div>
+  );
+}
+
+function Notification({ type }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
+      id="notification"
+    ></motion.div>
+  );
+}
